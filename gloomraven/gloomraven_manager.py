a3b2caa9eca5eec7ee85312d484297c2f0e98787
Vishengel <jellevisser1994@hotmail.com>
2025-01-10 09:18:59 +0100

    First full implementation on Philips hue element-based lights for all element types

--- gloomraven/gloomraven_manager.py
+++ gloomraven/gloomraven_manager.py
@@ -1,8 +1,11 @@
 import logging
 import sys
+from typing import Dict, List
 
 from gloomraven.config import BASECONFIG
 from gloomraven.data_model.element_state import Element, ElementLevel
+from gloomraven.data_model.game_state import GameState
+from gloomraven.smartlight_client.config import ELEMENT_COLOR_MAP
 from gloomraven.smartlight_client.philips_hue_client import PhilipsHueClient
 from gloomraven.x_haven_client.x_haven_client import XHavenClient
 
@@ -14,24 +17,58 @@ class GloomravenManager:
     def __init__(self, ip: str, port: int):
         self.x_haven_client = XHavenClient(ip, port)
         self.running = False
-        self.philips_hue_client = PhilipsHueClient("192.168.1.153")
+        self.philips_hue_client = PhilipsHueClient(
+            "192.168.1.153"
+        )  # Hard coded during testing phase
+        self.light_ids = [8, 9, 12]  # Hard coded during testing phase
+        self.current_active_elements: Dict[Element, ElementLevel] = {}
+        self.color_updates: List[Dict[str, int]] = []
 
     def run(self) -> None:
         self.running = True
         with self.x_haven_client.connect() as connection:
             while self.running:
-                try:
-                    data = self.x_haven_client.receive_data(connection)
-                    if data:
-                        game_state = self.x_haven_client.process_server_message(data)
-                        element_state = game_state.element_state
-                        if element_state[Element.FIRE] == ElementLevel.FULL:
-                            self.philips_hue_client.bridge.set_light([8,9,12], {"hue": 0, "sat": 254, "bri": 254})
-                        elif element_state[Element.FIRE] == ElementLevel.HALF:
-                            self.philips_hue_client.bridge.set_light([8, 9, 12], {"hue": 0, "sat": 254, "bri": 254})
-                        else:
-                            self.philips_hue_client.bridge.set_light([8,9,12], {"hue": 0, "sat": 100, "bri": 254})
+                self._await_and_process_data(connection)
 
-                except KeyboardInterrupt:
-                    logger.info("Terminating due to KeyboardInterrupt")
-                    self.running = False
+    def _await_and_process_data(self, connection):
+        try:
+            data = self.x_haven_client.receive_data(connection)
+            if data:
+                game_state = self.x_haven_client.process_server_message(data)
+                self._process_game_state(game_state)
+                self._update()
+        except KeyboardInterrupt:
+            logger.info("Terminating due to KeyboardInterrupt")
+            self.running = False
+
+    def _process_game_state(self, game_state: GameState):
+        self._process_element_state(game_state.element_state)
+
+    def _process_element_state(self, element_state: Dict[Element, ElementLevel]):
+        new_active_elements = {
+            element: element_level
+            for element, element_level in element_state.items()
+            if element_level != ElementLevel.INERT
+        }
+        if new_active_elements == self.current_active_elements:
+            return
+
+        for element, element_level in new_active_elements.items():
+            if element not in ELEMENT_COLOR_MAP:
+                logger.error(
+                    "No mapping to a HSV color is present in the element color map for element %s",
+                    element,
+                )
+                continue
+            hsv_for_element = ELEMENT_COLOR_MAP[element]
+
+            if element_level == ElementLevel.HALF:
+                hsv_for_element["bri"] = (
+                    hsv_for_element["bri"] // 2
+                )  # Half brightness if element is at half capacity
+
+    def _update(self):
+        # This is temporary, as this just cycles through the element-based colors and immediately
+        #  updates the lights for each.
+        for color in self.color_updates:
+            self.philips_hue_client.bridge.set_light(self.light_ids, color)
--- gloomraven/smartlight_client/config.py
+++ gloomraven/smartlight_client/config.py
@@ -3,14 +3,14 @@ from typing import Dict
 from gloomraven.config import BaseConfig
 from gloomraven.data_model.element_state import Element
 
-# Hardcoded from now, will be made configurable later
+# Hardcoded for now, will be made configurable later
 ELEMENT_COLOR_MAP = {
     Element.FIRE: {"hue": 0, "sat": 254, "bri": 254},  # Red
     Element.ICE: {"hue": 46920, "sat": 254, "bri": 254},  # Blue
     Element.EARTH: {"hue": 25500, "sat": 254, "bri": 254},  # Green
     Element.AIR: {"hue": 40000, "sat": 254, "bri": 254},  # Light blue
-    Element.LIGHT: {"hue": 40000, "sat": 254, "bri": 254},  # Placeholder
-    Element.DARK: {"hue": 40000, "sat": 254, "bri": 254},  # Placeholder
+    Element.LIGHT: {"hue": 12750, "sat": 200, "bri": 254},  # Warm yellowish-white
+    Element.DARK: {"hue": 50000, "sat": 254, "bri": 100},  # Deep purple
 }
 
 
